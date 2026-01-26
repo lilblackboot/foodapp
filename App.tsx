1f965616980be59d30ca81d3c0ec40e1bbb5d829
ritikkumar27 <kumarritikverma744@gmail.com>
2026-01-26 18:50:41 +0530

    Made status bar to be visible in both dark&light theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import {useColorScheme} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
     <SafeAreaProvider>
-      <StatusBar style="dark" />
+      <StatusBar style= {colorScheme === 'dark' ? 'light' : 'dark'} />
       <AppNavigator />
     </SafeAreaProvider>
   );
